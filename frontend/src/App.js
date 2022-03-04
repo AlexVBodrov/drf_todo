a1f72a3754c428d544996a62f1d00e607af2c300
AlexVBodrov <alexvbodrov@mail.ru>
2022-03-04 22:18:25 +0300

    Lesson 3. React start. add menu, foter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -38,9 +38,29 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div class="center">
+
           <h1>TODO App</h1>
+          <div>
+            <ul class="menu-main">
+              <li><a href="#">Главная</a></li>
+              <li><a href="#">Услуги</a></li>
+              <li><a href="#">Цены</a></li>
+              <li><a href="#">Контакты</a></li>
+            </ul>
+          </div>
+          
+          <br></br>
+          <br></br>
+          <br></br>
+
           < UserList users={this.state.users}/>
+
+          <br></br>
+
+          <footer id="footer">
+            <p>© 2022 Alex Bodrov</p>
+          </footer>
 
 
       </div>
