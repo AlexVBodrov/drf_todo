d105868db5825f57ec25a1887fcaa2caf7269edf
AlexVBodrov <alexvbodrov@mail.ru>
2022-03-27 18:08:49 +0300

    add model TODO

--- TODO/models.py
+++ TODO/models.py
@@ -1,3 +1,6 @@
+from pyexpat import model
+from tkinter import CASCADE
+from venv import create
 from django.db import models
 from users.models import User
 
@@ -7,3 +10,13 @@ class Project(models.Model):
     name = models.CharField(max_length=100, unique=True)
     link_repository = models.URLField(blank=True, null=True)
     users_list = models.ManyToManyField(User)
+
+
+class TODO(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    text = models.TextField()
+    created = models.DateTimeField(verbose_name="created", auto_now_add=True)
+    updated = models.DateTimeField(verbose_name="updated", auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(verbose_name="active", default=True)
+
