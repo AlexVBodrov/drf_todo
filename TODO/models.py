2fb6e1a9715644ad0c29ac6b3309a8fb5a1e1ba9
AlexVBodrov <alexvbodrov@mail.ru>
2022-03-27 17:20:49 +0300

    add model Project

--- TODO/models.py
+++ TODO/models.py
@@ -1,3 +1,10 @@
+from re import M
 from django.db import models
+from users.models import User
 
 # Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    link_repository = models.URLField(blank=True, null=True)
+    users_list = models.ManyToManyField(User)
