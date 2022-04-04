e1a9875af3c87b5bf72e84e8c855651fa4e8cc95
AlexVBodrov <alexvbodrov@mail.ru>
2022-04-05 01:13:54 +0300

    avto corector code Prettir

--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -1,49 +1,37 @@
-import React from "react";
+import React from 'react';
 
 import './components.css';
 
+const UserItem = ({ user }) => {
+  return (
+    // возвращаем верстку
+    <tbody>
+      <tr>
+        <td>{user.username}</td>
+        <td>{user.first_name}</td>
+        <td>{user.last_name}</td>
+        <td>{user.email}</td>
+      </tr>
+    </tbody>
+  );
+};
 
-const UserItem = ({user}) => {
-    return(
-        // возвращаем верстку
-        <tbody>
-            <tr>
-                <td>
-                    {user.username}
-                </td>
-                <td>
-                    {user.first_name}
-                </td>
-                <td>
-                    {user.last_name}
-                </td>
-                <td>
-                    {user.email}
-                </td>
-            </tr>
-        </tbody>
+const UserList = ({ users }) => {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Username</th>
+          <th>First name</th>
+          <th>Last name</th>
+          <th>email</th>
+        </tr>
+      </thead>
+      {users.map((user) => (
+        <UserItem user={user} />
+      ))}
+    </table>
+  );
+};
 
-    )
-}
-
-
-const UserList = ({users}) => {
-
-    return(
-        <table className="table">
-            <thead>
-                <tr>
-                    <th>Username</th>
-                    <th>First name</th>
-                    <th>Last name</th>
-                    <th>email</th>
-                </tr>
-
-            </thead>
-            {users.map((user) => <UserItem user={user}/> )}
-        </table>
-
-    )
-}
-
-export default UserList
+export default UserList;
