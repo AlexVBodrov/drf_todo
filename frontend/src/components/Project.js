59d6c809b9ee8b0d588e20f46b1d564624fdc179
AlexVBodrov <alexvbodrov@mail.ru>
2022-04-05 01:18:14 +0300

    avto corrector code

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -1,49 +1,37 @@
-import React from "react";
+import React from 'react';
 
 import './components.css';
 
+const ProjectItem = ({ project }) => {
+  return (
+    // возвращаем верстку
+    <tbody>
+      <tr>
+        <td>{project.id}</td>
+        <td>{project.name}</td>
+        <td>{project.users_list}</td>
+        <td>{project.link_repository}</td>
+      </tr>
+    </tbody>
+  );
+};
 
-const ProjectItem = ({project}) => {
-    return(
-        // возвращаем верстку
-        <tbody>
-            <tr>
-                <td>
-                    {project.id}
-                </td>
-                <td>
-                    {project.name}
-                </td>
-                <td>
-                    {project.users_list}
-                </td>
-                <td>
-                    {project.link_repository}
-                </td>
+const ProjectList = ({ projects }) => {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>NAME</th>
+          <th>Users list</th>
+          <th>Link</th>
+        </tr>
+      </thead>
+      {projects.map((project) => (
+        <ProjectItem project={project} />
+      ))}
+    </table>
+  );
+};
 
-            </tr>
-        </tbody>
-
-    )
-}
-
-
-const ProjectList = ({projects}) => {
-
-    return(
-        <table className="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>NAME</th>
-                    <th>Users list</th>
-                    <th>Link</th>
-                </tr>
-            </thead>
-            {projects.map((project) => <ProjectItem project={project}/> )}
-        </table>
-
-    )
-}
-
-export default ProjectList
+export default ProjectList;
