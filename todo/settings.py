4d9ce23061428d9baa7d3484260a4847b8ffb2f3
AlexVBodrov <alexvbodrov@mail.ru>
2022-04-05 23:36:44 +0300

    add Global DEFAULT_PERMISSION_CLASSES & fix some code

--- todo/settings.py
+++ todo/settings.py
@@ -137,22 +137,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 
 
-# REST_FRAMEWORK = {
-
-#     'DEFAULT_RENDERER_CLASSES': (
-#         'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
-#         'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
-#         # Any other renders
-#     ),
-
-#     'DEFAULT_PARSER_CLASSES': (
-#         # If you use MultiPartFormParser or FormParser, we also have a camel case version
-#         'djangorestframework_camel_case.parser.CamelCaseFormParser',
-#         'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
-#         'djangorestframework_camel_case.parser.CamelCaseJSONParser',
-#         # Any other parsers
-#     ),
-#     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-#     'PAGE_SIZE': 100
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
+}
