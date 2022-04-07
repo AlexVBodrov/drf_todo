7e84097f91eb63739e22aec7d5bad2c4229c39f4
AlexVBodrov <alexvbodrov@mail.ru>
2022-04-08 00:29:52 +0300

    add CustomUserAdmin

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 from .models import User
 # Register your models here.
 
-admin.site.register(User)
+# admin.site.register(User)
+
+from django.contrib.auth.admin import UserAdmin
+
+
+
+class CustomUserAdmin(UserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {"classes": ("wide",), "fields": ("username", "password1", "password2", "email")},
+        ),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
