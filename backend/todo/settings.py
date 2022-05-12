eb6fa2c1bdd0c4dc584f89c4178c5ce7629d9b0e
AlexVBodrov <alexvbodrov@mail.ru>
2022-05-12 16:19:46 +0300

    fix many Errors. Docker Backend => RUN, Frontend => RUN.

--- backend/todo/settings.py
+++ backend/todo/settings.py
@@ -30,6 +30,9 @@ ALLOWED_HOSTS = []
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:8080',
+    'http://127.0.0.1:80',
 ]
 
 # Application definition
